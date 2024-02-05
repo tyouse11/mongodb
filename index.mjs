ba80d03ce75d2306eae3c8d6d4d44d3b1c3397dd
tyouse1 <tyouse1@live.com>
2024-02-05 10:47:55 -0500

    added post and delete request

--- index.mjs
+++ index.mjs
@@ -1,11 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import mongoose from "mongoose";
+import grades from "./routes/grades.mjs"
+
+// Connection string
+const ATLAS_URI = process.env.ATLAS_URI
+const db = mongoose.connection
+// Conncect to MongoDB through Mongoose
+mongoose.connect(ATLAS_URI)
+
+// Connection Error/Success
+// Define callback functions for various events
+db.on("error", (err) => console.log(err.message + " is mongod not running?"));
+db.on("open", () => console.log("mongo connected: ", ATLAS_URI));
+db.on("close", () => console.log("mongo disconnected"));
 
 const PORT = process.env.PORT || 5050;
 const app = express();
-
-import grades from "./routes/grades.mjs"
 
 app.use(express.json());
 
--- routes/grades.mjs
+++ routes/grades.mjs
@@ -1,8 +1,18 @@
 import express from "express";
 import db from "../db/conn.mjs";
 import { ObjectId } from "mongodb"; // comes from MongoDB library
+import Grade from '../models/grade.mjs';
 
 const router = express.Router();
+
+
+// Index - Find all grades
+router.get('/', async (req, res) => {
+  let foundGrades = await Grade.find().limit(50)
+  res.status(200).json({
+    foundGrades: foundGrades
+  })
+})
 
 // Create a single grade entry
 router.post("/", async (req, res) => {
@@ -21,13 +31,20 @@ router.post("/", async (req, res) => {
 
 // Get a single grade entry
 router.get("/:id", async (req, res) => {
-    let collection = await db.collection("grades");
-    let query = { _id: new ObjectId(req.params.id) };
-    let result = await collection.findOne(query);
+    // for MongoDB
+    // let collection = await db.collection("grades");
+    // let query = { _id: new ObjectId(req.params.id) };
+    // let result = await collection.findOne(query);
 
-    if (!result) res.send("Not found").status(404);
-    else res.send(result).status(200);
-});
+    // if (!result) res.send("Not found").status(404);
+    // else res.send(result).status(200);
+
+    // for Mongoose
+    let foundGrade = await Grade.findById(req.params.id)
+    res.status(200).json({
+      data: foundGrade
+    })
+  });
 
 // Add a score to a grade entry
 router.patch("/:id/add", async (req, res) => {
@@ -57,13 +74,25 @@ router.patch("/:id/add", async (req, res) => {
   
   // Delete a single grade entry
   router.delete("/:id", async (req, res) => {
-    let collection = await db.collection("grades");
-    let query = { _id: ObjectId(req.params.id) };
-    let result = await collection.deleteOne(query);
+    // let collection = await db.collection("grades");
+    // let query = { _id: ObjectId(req.params.id) };
+    // let result = await collection.deleteOne(query);
   
-    if (!result) res.send("Not found").status(404);
-    else res.send(result).status(200);
+    // if (!result) res.send("Not found").status(404);
+    // else res.send(result).status(200);
+    await Grade.findByIdAndDelete(req.params.id) 
+    res.status(204).json({
+      data: "Item has been deleted"
+    })
   });
+
+  router.patch("/:id", async (req, res) => {
+    const updatedGrade = await Grade.findByIdAndUpdate(req.params.id, req.body, { new: true});
+    if (!updatedGrade) {
+      return res.status(404).json({ error: 'Grade not found'});
+    }
+    res.status(200).json(updatedGrade);
+  })
 
 // Student route for backwards compatibility
 router.get("/student/:id", async (req, res) => {
@@ -72,12 +101,38 @@ router.get("/student/:id", async (req, res) => {
 
 // Get a learner's grade data
 router.get("/learner/:id", async (req, res) => {
-    let collection = await db.collection("grades");
-    let query = { learner_id: Number(req.params.id) };
-    let result = await collection.find(query).toArray();
+    // let collection = await db.collection("grades");
+    // let query = { learner_id: Number(req.params.id) };
+    // let result = await collection.find(query).toArray();
   
-    if (!result) res.send("Not found").status(404);
-    else res.send(result).status(200);
+    // // Check for class_id parameter 
+    // if (req.query.class) query.class_id = Number(req.query.class)
+
+    
+    // if (!result) res.send("Not found").status(404);
+    // else res.send(result).status(200);
+    // let learner_id = await Grade.findOne(req.params.id)
+    // if (!learnerGrade) {
+    //   return res.status(404).json({ error: 'Learner not found' });
+    // }
+    // res.status(200).json({
+    //   data: learner_id 
+    // })
+    let result = await Grade.find({learner_id: req.params.id})
+
+    if (!result) res.status(404).json({ data: "Not found"})
+    else res.status(200).json({
+      foundLearner: result});
+  });
+
+  // Delete a learner by learner_id
+  router.delete("/learner/:id", async (req, res) => {
+    let query = { learner_id: req.params.id};
+
+    let result = await Grade.find(query);
+    result.forEach( async ( grade, i) => {
+      await Grade.findByIdAndDelete(grade._id)
+    })
   });
 
   // Get a class's grade data
